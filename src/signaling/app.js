ae834af12978fac946aaab17bec7bfeeb1c14130
dagai <602931725@qq.com>
2019-01-05 13:34:12 +0800

    init node project

---
+++ src/signaling/app.js
@@ -0,0 +1,74 @@
+/*
+ * File: app.js
+ * Project: future stream
+ * Created Date: Saturday January 5th 2019
+ * Author: DaGai  <binghan2836@163.com>
+ * -----
+ * Last Modified: Saturday January 5th 2019 2:16:36 am
+ * Modified By:   the developer formerly known as DaGai
+ * -----
+ * MIT License
+ * 
+ * Copyright (c) 2019 binghan2836@163.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * -----
+ * HISTORY:
+ * Date          By    Comments
+ * ----------    ---    ----------------------------------------------------------
+ * 2019-01-05    DaGai    
+ */
+
+
+/* eslint-disable no-console */
+
+var a = 'a';
+
+/**
+ * @description 
+ * @param 
+ * @param {*} arg 
+ */
+function test(arg) {
+    console.log(arg);
+    return 'ddd';
+}
+console.log(a);
+
+console.log(test);
+
+/**
+ * 
+ * @param {*} val 
+ */
+function Myclass(val) {
+    this.value = val;
+}
+
+Myclass.prototype.minus = function(){
+    this.value --;
+};
+
+Myclass.add = function (){
+    this.value ++;
+};
+
+var myclass = new Myclass(2);
+
+myclass.add();
